cb21e862b46d6ce252a6f3900a9a3d5412a1e222
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-07-27 12:48:42 +0400

    Changes to card info stack design and added localization support for CPP page

--- Sources/OderoPay/View/CommonPaymentPageViewController.swift
+++ Sources/OderoPay/View/CommonPaymentPageViewController.swift
@@ -10,13 +10,92 @@ import UIKit
 
 public class CommonPaymentPageViewController: UIViewController {
     
-    
+    // UILabels
     @IBOutlet weak var totalPriceLabel: UILabel!
+    @IBOutlet weak var paymentMethodLabel: UILabel!
+    @IBOutlet weak var creditCardOrDebitCardLabel: UILabel!
+    @IBOutlet weak var multipleCreditCardsLabel: UILabel!
+    
+    // UITextFields
+    @IBOutlet weak var cardNumberTextField: UITextField!
+    @IBOutlet weak var monthYearTextField: UITextField!
+    @IBOutlet weak var cvcTextField: UITextField!
+    @IBOutlet weak var cardholderNameTextField: UITextField!
+    
+    // UIButtons
+    @IBOutlet weak var confirmPaymentButton: UIButton!
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        totalPriceLabel.text = NSLocalizedString("totalPrice", bundle: Bundle.module, comment: "totalPriceLabel")
+        
+        // -----------------LOCALIZATIONS-----------------
+        
+        // setting up label text localizations
+        totalPriceLabel.text = NSLocalizedString("totalPrice",
+                                                 bundle: Bundle.module,
+                                                 comment: "total price to be paid")
+        
+        paymentMethodLabel.text = NSLocalizedString("paymentMethod",
+                                                    bundle: Bundle.module,
+                                                    comment: "payment method for checkout")
+        
+        creditCardOrDebitCardLabel.text = NSLocalizedString("creditCardOrDebitCard",
+                                                            bundle: Bundle.module,
+                                                            comment: "pay with credit card or debit card")
+        
+        multipleCreditCardsLabel.text = NSLocalizedString("multipleCredit",
+                                                          bundle: Bundle.module,
+                                                          comment: "pay with credit card or debit card")
+        
+        // setting up textfield placeholder localizations
+        cardNumberTextField.placeholder = NSLocalizedString("cardNumber",
+                                                            bundle: Bundle.module,
+                                                            comment: "card number")
+        
+        monthYearTextField.placeholder = NSLocalizedString("mm/yy",
+                                                           bundle: Bundle.module,
+                                                           comment: "card expire month and year")
+        
+        cvcTextField.placeholder = NSLocalizedString("cvc",
+                                                     bundle: Bundle.module,
+                                                     comment: "card cvc code")
+        
+        cardholderNameTextField.placeholder = NSLocalizedString("cardHolderNameSurname",
+                                                                bundle: Bundle.module,
+                                                                comment: "card holder's name and surname")
+        
+        // setting up button text localizations
+        confirmPaymentButton.setTitle(
+            NSLocalizedString("makePayment",
+                              bundle: Bundle.module,
+                              comment: "make payment with chosen payment method"),
+            for: .normal)
+        
+        // -----------------UI CHANGES-----------------
+        
+        // credit or debit card number
+        let cardNumberLeftView = UIImageView()
+        let cardImage = UIImage(systemName: "creditcard")
+        cardNumberLeftView.image = cardImage
+        cardNumberTextField.leftView = cardNumberLeftView
+        
+        // card expire month year
+        let expireDateLeftYear = UIImageView()
+        let calendarImage = UIImage(systemName: "calendar")
+        expireDateLeftYear.image = calendarImage
+        cardNumberTextField.leftView = expireDateLeftYear
+        
+        // card secure code
+        let cvcCodeLeftView = UIImageView()
+        let lockImage = UIImage(systemName: "lock")
+        cvcCodeLeftView.image = lockImage
+        cardNumberTextField.leftView = cvcCodeLeftView
+        
+        // card holder name surname
+        let cardHolderNameLeftView = UIImageView()
+        let personImage = UIImage(systemName: "person")
+        cardHolderNameLeftView.image = personImage
+        cardNumberTextField.leftView = cardHolderNameLeftView
     }
     
 }
