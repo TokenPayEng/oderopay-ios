7899e48a1e1764684eba283b9f6f3670d887898f
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-07-28 10:51:00 +0400

    adding new locales and odero button

---
+++ Sources/OderoPay/View/OderoPayButtonView.swift
@@ -0,0 +1,48 @@
+//
+//  OderoPayButtonView.swift
+//  
+//
+//  Created by Imran Hajiyev on 28.07.22.
+//
+
+import UIKit
+
+public class OderoPayButtonView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var oderoPayButton: UIButton! {
+        didSet {
+            oderoPayButton.layer.cornerRadius = 6
+            
+            oderoPayButton.setTitle(
+                NSLocalizedString(
+                    "payWithOdero",
+                    bundle: Bundle.module,
+                    comment: "pay with odero button"
+                ),
+                for: .normal)
+        }
+    }
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.module.loadNibNamed("OderoPayButtonView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+    @IBAction func initCommonPaymentPage(_ sender: Any) {
+        print("initializing common payment page")
+    }
+}
