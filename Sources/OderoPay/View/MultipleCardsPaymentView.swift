2e9539c1a044f9f662af7d07da6e74f85971fcb8
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-08-02 13:19:06 +0400

    -

---
+++ Sources/OderoPay/View/MultipleCardsPaymentView.swift
@@ -0,0 +1,29 @@
+//
+//  MultipleCardsPaymentView.swift
+//  
+//
+//  Created by Imran Hajiyev on 02.08.22.
+//
+
+import UIKit
+
+class MultipleCardsPaymentView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.module.loadNibNamed("MultipleCardsPayment", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+
+}
