e78d91196a17d59bd7ad3d8f096d07809a3d9026
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-08-03 12:01:15 +0400

    -

--- Sources/OderoPay/View/OderoPayButtonView.swift
+++ Sources/OderoPay/View/OderoPayButtonView.swift
@@ -15,7 +15,7 @@ public class OderoPayButtonView: UIView {
     @IBOutlet weak var oderoPayImageView: UIImageView!
     @IBOutlet weak var oderoPayButton: UIButton! {
         didSet {
-            oderoPayButton.layer.cornerRadius = 6
+            oderoPayButton.layer.cornerRadius = 10
         }
     }
     
