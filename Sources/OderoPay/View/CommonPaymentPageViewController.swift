f0fec3df0a125895c3949f16778c4b746a1f72c3
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-08-15 17:56:51 +0400

    -

--- Sources/OderoPay/View/CommonPaymentPageViewController.swift
+++ Sources/OderoPay/View/CommonPaymentPageViewController.swift
@@ -82,6 +82,8 @@ public class CommonPaymentPageViewController: UIViewController {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             if self.view.frame.origin.y == 0 {
                 self.view.frame.origin.y -= keyboardSize.height
+            } else {
+                self.view.frame.origin.y += keyboardSize.height
             }
         }
     }
