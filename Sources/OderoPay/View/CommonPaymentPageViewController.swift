9065c5562ac8a015fddfaaac60e1df00d17fc1c0
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-08-15 13:20:02 +0400

    scrolling bug fixes

--- Sources/OderoPay/View/CommonPaymentPageViewController.swift
+++ Sources/OderoPay/View/CommonPaymentPageViewController.swift
@@ -74,6 +74,22 @@ public class CommonPaymentPageViewController: UIViewController {
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
     
     override public func viewWillAppear(_ animated: Bool) {
