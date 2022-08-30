a5deac58b7700f79dd7dec45f2510cba9fe6c08d
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-08-30 11:26:05 +0400

    -

--- Sources/OderoPay/Model/Enum/Currency.swift
+++ Sources/OderoPay/Model/Enum/Currency.swift
@@ -16,21 +16,8 @@ public enum Currency: String, Codable {
 
 extension Currency {
     
-    enum Key: CodingKey {
-        case rawValue
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: Key.self)
-        switch self {
-        case .TRY:
-            try container.encode("TRY", forKey: .rawValue)
-        case .AZN:
-            try container.encode("AZN", forKey: .rawValue)
-        case .USD:
-            try container.encode("USD", forKey: .rawValue)
-        case .EURO:
-            try  container.encode("EURO", forKey: .rawValue)
-        }
+    enum Key: String, CodingKey {
+        case TRY = "TRY"
+        case AZN = "AZN"
     }
 }
