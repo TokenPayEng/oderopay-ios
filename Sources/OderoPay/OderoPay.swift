887b361a2362291aa0906dcd1ea085994d6de54a
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-08-30 11:41:50 +0400

    -

--- Sources/OderoPay/OderoPay.swift
+++ Sources/OderoPay/OderoPay.swift
@@ -54,15 +54,14 @@ public struct OderoPay {
         request.setValue(randomKey, forHTTPHeaderField: "x-rnd-key")
         request.setValue(signature, forHTTPHeaderField: "x-signature")
         request.setValue("V1", forHTTPHeaderField: "x-auth-version")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        let encoded = try JSONEncoder().encode(OderoPay.checkoutForm)
+        let encodedBody = try JSONEncoder().encode(OderoPay.checkoutForm)
 //        let json = try JSONSerialization.jsonObject(with: encoded, options: [])
 //        guard let jsonString = String(data: encoded, encoding: .utf8) else { throw CheckoutError.invalidRequestBody }
 //        guard let httpRequestBody = jsonString.data(using: .utf8) else { throw CheckoutError.invalidRequestBody }
         
-        request.httpBody = encoded
-        
-        print(String(decoding: request.httpBody!, as: UTF8.self))
+        request.httpBody = encodedBody
         
         let (data, _) = try await URLSession.shared.data(with: request)
         return try JSONDecoder().decode(CheckoutFormResult.self, from: data)
