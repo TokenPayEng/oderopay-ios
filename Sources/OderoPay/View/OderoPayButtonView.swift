75762cd25e931842e230fb6ffdec1cd8ad951f59
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-08-31 12:01:58 +0400

    -

--- Sources/OderoPay/View/OderoPayButtonView.swift
+++ Sources/OderoPay/View/OderoPayButtonView.swift
@@ -102,6 +102,7 @@ public class OderoPayButtonView: UIView, SFSafariViewControllerDelegate {
             if OderoPay.isCheckoutFormReady() {
                 print("checkout form found ---- SUCCESS ✅\n")
                 
+                oderoPayButton.loadingIndicator(true)
                 print("STEP #3 ---- (NETWORK)")
                 print("generating random key...")
                 print("sending checkout form...")
@@ -130,6 +131,8 @@ public class OderoPayButtonView: UIView, SFSafariViewControllerDelegate {
                         print(token)
                         print("checkout form sent ---- SUCCESS ✅\n")
                         OderoPay.assignRetrievedToken(withValue: token)
+                        
+                        oderoPayButton.loadingIndicator(false)
                         
                         print("STEP #4 ---- (LOCAL)")
                         print("retrieving displaying view settings...")
@@ -164,6 +167,30 @@ public class OderoPayButtonView: UIView, SFSafariViewControllerDelegate {
         } else {
             print("no keys were provided by developer ---- FAIL ❌")
             return
+        }
+    }
+}
+
+extension UIButton {
+    func loadingIndicator(_ show: Bool) {
+        let tag = 808404
+        if show {
+            self.isEnabled = false
+            self.alpha = 0.5
+            let indicator = UIActivityIndicatorView()
+            let buttonHeight = self.bounds.size.height
+            let buttonWidth = self.bounds.size.width
+            indicator.center = CGPoint(x: buttonWidth/2, y: buttonHeight/2)
+            indicator.tag = tag
+            self.addSubview(indicator)
+            indicator.startAnimating()
+        } else {
+            self.isEnabled = true
+            self.alpha = 1.0
+            if let indicator = self.viewWithTag(tag) as? UIActivityIndicatorView {
+                indicator.stopAnimating()
+                indicator.removeFromSuperview()
+            }
         }
     }
 }
