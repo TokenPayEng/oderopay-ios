8617bb70682417d242bcb2c2af39e4a318939753
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-08-31 15:45:41 +0400

    -

--- Sources/OderoPay/View/CommonPaymentPageViewController.swift
+++ Sources/OderoPay/View/CommonPaymentPageViewController.swift
@@ -119,7 +119,7 @@ public class CommonPaymentPageViewController: UIViewController {
     }
     
     @IBAction func collapseCreditOrDebitSection(_ sender: Any) {
-        collapseSection(creditOrDebitCardView, ofHeight: creditOrDebitCardView.height, using: creditCardOrDebitCardButton, and: creditOrDebitCardViewHeightConstraint)
+        collapseSection(creditOrDebitCardView, ofHeight: CardInformationView.height, using: creditCardOrDebitCardButton, and: creditOrDebitCardViewHeightConstraint)
     }
     
     @IBAction func collapseMultipleCreditSection(_ sender: Any) {
