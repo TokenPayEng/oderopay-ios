2fff413df6ec82175aa8c490101c6abc5639d07a
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-09-01 16:57:11 +0400

    -

--- Sources/OderoPay/View/CardInformationView.swift
+++ Sources/OderoPay/View/CardInformationView.swift
@@ -436,6 +436,7 @@ extension CardInformationView {
         chooseInstallmentLabel.font = UIFont.systemFont(ofSize: 14, weight: .medium)
         coreStackView.addArrangedSubview(chooseInstallmentLabel)
         coreStackView.layoutIfNeeded()
+        CommonPaymentPageViewController().creditOrDebitCardViewHeightConstraint = coreStackView.frame.size.height
         print(chooseInstallmentLabel.frame.height)
         print("+------------------------+")
         print(coreStackView.frame.height)
