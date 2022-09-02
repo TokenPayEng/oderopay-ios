ececc6255833240221f5812322dadae754474995
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-09-02 15:07:31 +0400

    -

--- Sources/OderoPay/View/OptionsView.swift
+++ Sources/OderoPay/View/OptionsView.swift
@@ -9,7 +9,27 @@ import UIKit
 
 class OptionsView: UIView {
 
+    private let tapped3DS = UITapGestureRecognizer(target: OptionsView.self, action: #selector(threeDSClicked))
+    private let tappedSaveCard = UITapGestureRecognizer(target: OptionsView.self, action: #selector(saveCardClicked))
+    private var threeDSSelected: Bool = false
+    private var saveCardSelected: Bool = false
+    
     @IBOutlet var contentView: UIView!
+    
+    @IBOutlet weak var threeDSChoiceStackView: UIStackView!
+    @IBOutlet weak var saveCardChoiceStackView: UIStackView!
+    
+    @IBOutlet weak var threeDSLabel: UILabel! {
+        didSet {
+            threeDSLabel.text = NSLocalizedString("3dsPrompt", bundle: .module, comment: "Use 3DS Prompt")
+        }
+    }
+    
+    @IBOutlet weak var saveCardLabel: UILabel! {
+        didSet {
+            saveCardLabel.text = NSLocalizedString("saveCardPrompt", bundle: .module, comment: "Save Card Prompt")
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,5 +46,29 @@ class OptionsView: UIView {
         addSubview(contentView)
         contentView.frame = self.bounds
         contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+    @objc func threeDSClicked() {
+        
+        if threeDSSelected {
+            checkImageView.image = UIImage(systemName: "checkmark.square")
+            checkImageView.tintColor = UIColor.init(red: 53, green: 211, blue: 47, alpha: 1)
+        } else {
+            checkImageView.image = UIImage(named: "square")
+            checkImageView.tintColor = UIColor.init(red: 225, green: 225, blue: 225, alpha: 1)
+        }
+    
+    }
+    
+    @objc func saveCardClicked() {
+        
+        if saveCardSelected {
+            checkImageView.image = UIImage(systemName: "checkmark.square")
+            checkImageView.tintColor = UIColor.init(red: 53, green: 211, blue: 47, alpha: 1)
+        } else {
+            checkImageView.image = UIImage(named: "square")
+            checkImageView.tintColor = UIColor.init(red: 225, green: 225, blue: 225, alpha: 1)
+        }
+    
     }
 }
