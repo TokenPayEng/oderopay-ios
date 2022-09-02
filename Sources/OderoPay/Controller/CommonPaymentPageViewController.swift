21564f50fcb745db8dfd6fb376005ff9b48345c8
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-09-02 15:26:32 +0400

    -

--- Sources/OderoPay/Controller/CommonPaymentPageViewController.swift
+++ Sources/OderoPay/Controller/CommonPaymentPageViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public class CommonPaymentPageViewController: UIViewController {
     
-    var creditOrDebitPaymentViewHeight: CGFloat = 260
+    var creditOrDebitPaymentViewHeight: CGFloat = 400
     
     // ---------------------UIViews----------------------
     @IBOutlet weak var creditOrDebitCardView: CreditOrDebitCardPaymentView!
