4526e1e4805b02224ecc8a50e471d07af0e8d1fc
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-09-07 18:16:55 +0400

    -

--- Sources/OderoPay/View/CreditOrDebitCardPaymentView.swift
+++ Sources/OderoPay/View/CreditOrDebitCardPaymentView.swift
@@ -144,7 +144,7 @@ class CreditOrDebitCardPaymentView: UIView {
         OderoPay.setCompletePaymentForm(to: form)
         Task {
             do {
-                print(try await OderoPay.sendCheckoutForm())
+                print(try await OderoPay.sendCompletePaymentForm()
             } catch {
                 print(error)
             }
