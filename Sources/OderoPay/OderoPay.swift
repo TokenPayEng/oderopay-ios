5aad9e939983732467131485d7d7c850efe7175e
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-09-09 17:39:05 +0400

    -

--- Sources/OderoPay/OderoPay.swift
+++ Sources/OderoPay/OderoPay.swift
@@ -101,7 +101,7 @@ public struct OderoPay {
         // header default
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        print("randomKey")
+        print(randomKey)
         
         // send request
         let (data, _) = try await URLSession.shared.data(with: request)
