1ce5de3cab7cd8997d29f99d364f090710c16822
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-09-13 16:45:31 +0400

    -

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "OderoPay",
-    defaultLocalization: "en",
+    defaultLocalization: "ru",
     platforms: [
         .iOS(.v13)
     ],
