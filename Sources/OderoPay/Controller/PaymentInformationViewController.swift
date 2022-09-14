8a961497ceeaac600a82ce1850a723b02ecf84f5
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-09-14 12:16:00 +0400

    -

--- Sources/OderoPay/Controller/PaymentInformationViewController.swift
+++ Sources/OderoPay/Controller/PaymentInformationViewController.swift
@@ -40,6 +40,7 @@ class PaymentInformationViewController: UIViewController {
     }
 
     @IBAction func backToCheckout(_ sender: Any) {
+        print(self.parent?.navigationController)
         self.parent?.navigationController?.popViewController(animated: true)
         self.dismiss(animated: true)
     }
