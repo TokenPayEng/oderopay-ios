ead163f5657aa6a684340c014ff455736d7b066b
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-09-14 11:58:46 +0400

    updates to ui (complete payment)

--- Sources/OderoPay/Controller/PaymentInformationViewController.swift
+++ Sources/OderoPay/Controller/PaymentInformationViewController.swift
@@ -12,6 +12,7 @@ class PaymentInformationViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel2: UILabel!
     @IBOutlet weak var button: UIButton!
     
     override func viewDidLoad() {
@@ -23,13 +24,16 @@ class PaymentInformationViewController: UIViewController {
             imageView.image = UIImage(named: "success", in: .module, with: .none)
             titleLabel.text = NSLocalizedString("paymentSuccess", bundle: .module, comment: "on successful payment")
             subtitleLabel.text = NSLocalizedString("returnToHome", bundle: .module, comment: "on successful payment")
+            
+            subtitleLabel2.isHidden = true
             button.isHidden = true
         } else {
             imageView.image = UIImage(named: "error", in: .module, with: .none)
-            titleLabel.text = NSLocalizedString("paymentError", bundle: .module, comment: "on successful payment")
-            subtitleLabel.text = NSLocalizedString("paymentErrorHelp", bundle: .module, comment: "on successful payment")
+            titleLabel.text = NSLocalizedString("paymentError", bundle: .module, comment: "on failure")
+            subtitleLabel.text = NSLocalizedString("paymentErrorHelp", bundle: .module, comment: "on failure")
+            subtitleLabel2.text = NSLocalizedString("paymentErrorHelp2", bundle: .module, comment: "on failure")
             button.setTitle(
-                NSLocalizedString("returnToCheckout", bundle: .module, comment: "on failure"),
+                NSLocalizedString("returnToCheckout", bundle: .module, comment: "on failure").uppercased(),
                 for: .normal
             )
         }
