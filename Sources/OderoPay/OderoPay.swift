baac37ca0c8d567bfb63cdfe3e79f2c4c5ff9a72
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-10-05 13:06:51 +0400

    latest stable

--- Sources/OderoPay/OderoPay.swift
+++ Sources/OderoPay/OderoPay.swift
@@ -260,7 +260,6 @@ public struct OderoPay {
         
         // send request
         let (data, _) = try await URLSession.shared.data(with: request)
-        print(String(data: data, encoding: .utf8) as Any)
         return try JSONDecoder().decode(CompletePaymentFormResult.self, from: data)
     }
     
@@ -294,7 +293,6 @@ public struct OderoPay {
         
         // send request
         let (data, _) = try await URLSession.shared.data(with: request)
-        print(String(data: data, encoding: .utf8) as Any)
         return try JSONDecoder().decode(CompletePaymentFormResult.self, from: data)
     }
 }
