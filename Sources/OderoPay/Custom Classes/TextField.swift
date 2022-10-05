8342c310b33aab111831039fe29d2b85a0a3c4fd
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-10-05 10:12:22 +0400

    disable all options

--- Sources/OderoPay/Custom Classes/TextField.swift
+++ Sources/OderoPay/Custom Classes/TextField.swift
@@ -10,9 +10,6 @@ import UIKit
 
 class TextField: UITextField {
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        if action == #selector(UIResponderStandardEditActions.paste(_:)) {
-            return false
-        }
-        return super.canPerformAction(action, withSender: sender)
+        return false
     }
 }
