82914810e752a920785dfa7ffb835f6f7e9f8edb
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-10-07 13:55:09 +0400

    payment method typo

--- Sources/OderoPay/Model/Enum/Payment/PaymentMethod.swift
+++ Sources/OderoPay/Model/Enum/Payment/PaymentMethod.swift
@@ -9,5 +9,5 @@ import Foundation
 
 public enum PaymentMethod: String, Codable {
     case CARD_PAYMENT = "CARD_PAYMENT"
-    case MULTI_CARD_PAYMENT = "MULTICARD_PAYMENT"
+    case MULTI_CARD_PAYMENT = "MULTI_CARD_PAYMENT"
 }
