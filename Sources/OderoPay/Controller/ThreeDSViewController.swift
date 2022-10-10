2e76badd21837fe4a8cccbdbda984b4ca2e124df
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-10-10 20:28:45 +0400

    -

--- Sources/OderoPay/Controller/ThreeDSViewController.swift
+++ Sources/OderoPay/Controller/ThreeDSViewController.swift
@@ -24,16 +24,12 @@ class ThreeDSViewController: UIViewController, WKNavigationDelegate {
 
         webView.loadHTMLString(htmlContent, baseURL: nil)
         webView.addObserver(self, forKeyPath: "URL", options: .new, context: nil)
+        navigationController?.title = "3DS Verification"
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if let key = change?[NSKeyValueChangeKey.newKey] {
             print("observeValue \(key)") // url value
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
 }
