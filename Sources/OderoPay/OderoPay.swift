be6dd69e130fbf938a71dc142758f033cfddb75a
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-10-10 10:46:19 +0400

    -

--- Sources/OderoPay/OderoPay.swift
+++ Sources/OderoPay/OderoPay.swift
@@ -219,13 +219,14 @@ public struct OderoPay {
         var urlComponents = URLComponents(string: environment.getGateway().getBaseURL() + Path.COMMON_PAYMENT_PAGE.rawValue + Action.INSTALLMENTS.rawValue)!
         urlComponents.queryItems = [
             URLQueryItem(name: "binNumber", value: binNumber),
+            URLQueryItem(name: "price", value: String(price)),
         ]
         
         var request = URLRequest(url: urlComponents.url!)
         
         // generate signature
         let signature = try generateSignature(for: urlComponents.url!.absoluteString, body: String())
-        print(urlComponents.url!.absoluteString)
+        
         // method
         request.httpMethod = HTTPMethod.GET.rawValue
         
