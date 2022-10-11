42e2ebad6800bbc9bbb9cdd7a71236b99169367d
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-10-11 17:16:38 +0400

    bug fixes

--- Sources/OderoPay/Model/Enum/Installment.swift
+++ Sources/OderoPay/Model/Enum/Installment.swift
@@ -11,7 +11,13 @@ public enum Installment: Int, Codable {
     case single = 1
     case double = 2
     case triple = 3
+    case quadruple = 4
+    case quintaple = 5
     case sextuple = 6
+    case septuple = 7
+    case octople = 8
     case nonuple = 9
+    case decuple = 10
+    case undecuple = 11
     case dozen = 12
 }
